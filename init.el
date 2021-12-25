28b577b6816a40829a6c65137c39819be5bf26b5
Merazi <merazi_pub@riseup.net>
2021-12-25 12:55:32 -0600

    Several changes
    - New package repository available
    - Now using liberation mono as my font
    - Changed the instruction for the Emoji Font
    - New key binding for inserting yasnippets
    - New key bindings for resizing emacs windows
    - Scroll bar is disabled
    - Yasnippet
    - Doom themes are back
    - I'm giving treemacs a try, the configuration in init.el is taken from the
      example available in the treemacs repository

--- init.el
+++ init.el
@@ -32,15 +32,16 @@
 
 ;; Enable melpa (I missed this)
 (require 'package)
-(add-to-list 'package-archives
-	     '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
 
 ;; set the global font
-(setq default-frame-alist '((font . "Cascadia Mono PL-10")))
+(setq default-frame-alist '((font . "Liberation Mono-10")))
 
 ;; display color emojis using a special font
-(set-fontset-font t '(#x1f000 . #x1faff)
-		  (font-spec :family "Noto Color Emoji"))
+(set-fontset-font t 'symbol "Noto Color Emoji" nil 'append)
+;; (set-fontset-font t '(#x1f000 . #x1faff)
+;;		  (font-spec :family "Noto Color Emoji"))
 
 ;; quality of editing tweaks
 (setq visible-bell 1
@@ -102,17 +103,19 @@
 (eval-after-load "dired"
   '(progn (define-key dired-mode-map (kbd "M-o") 'other-window)
 	  (define-key dired-mode-map (kbd "\C-co") 'mer/xdg-open)))
+(global-set-key "\C-cy" 'yas-insert-snippet)
 
 ;; easier window resizing
-(global-set-key (kbd "S-C-<left>") 'shrink-window-horizontally)
-(global-set-key (kbd "S-C-<right>") 'enlarge-window-horizontally)
-(global-set-key (kbd "S-C-<down>") 'shrink-window)
-(global-set-key (kbd "S-C-<up>") 'enlarge-window)
+(global-set-key (kbd "C-c <left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-c <right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-c <down>") 'shrink-window)
+(global-set-key (kbd "C-c <up>") 'enlarge-window)
 
 ;; don't send emacs to the background by mistake
 (global-unset-key (kbd "C-z"))
 
 ;; scroll one line at a time (less "jumpy" than defaults)
+(scroll-bar-mode -1)
 (setq mouse-wheel-scroll-amount '(1 ((shift) . 1)))
 (setq mouse-wheel-progressive-speed nil)
 (setq mouse-wheel-follow-mouse 't)
@@ -149,15 +152,20 @@
 
 ;; some packages
 (use-package ox-reveal
-:ensure ox-reveal)
+  :ensure t)
 (setq org-reveal-root "http://cdn.jsdelivr.net/reveal.js/3.0.0/")
 (setq org-reveal-mathjax t)
 (use-package htmlize
-:ensure t)
+  :ensure t)
 (use-package skewer-mode :ensure t)
-(use-package doom-themes :ensure t
+(use-package yasnippet-classic-snippets :ensure t)
+(use-package yasnippet :ensure t
   :config
-  (load-theme 'doom-oceanic-next t))
+  (yas-global-mode 1))
+(use-package modus-themes :ensure t
+  :config
+  (load-theme 'modus-operandi t))
+(use-package doom-themes :ensure t)
 (use-package simple-modeline :ensure t)
 (use-package yasnippet-snippets :ensure t)
 (use-package impatient-mode :ensure t)
@@ -202,3 +210,82 @@
   (setq org-static-blog-index-front-matter
 	"<h1>Welcome to my webpage 😊</h1>
 	 <div class=\"merazi_pfp\"><img src=\"media/merazi.jpg\"></div>\n"))
+(use-package treemacs
+  :ensure t
+  :defer t
+  :init
+  (with-eval-after-load 'winum
+    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
+  :config
+  (progn
+    (setq treemacs-collapse-dirs                   (if treemacs-python-executable 3 0)
+	  treemacs-deferred-git-apply-delay        0.5
+	  treemacs-directory-name-transformer      #'identity
+	  treemacs-display-in-side-window          t
+	  treemacs-eldoc-display                   t
+	  treemacs-file-event-delay                5000
+	  treemacs-file-extension-regex            treemacs-last-period-regex-value
+	  treemacs-file-follow-delay               0.2
+	  treemacs-file-name-transformer           #'identity
+	  treemacs-follow-after-init               t
+	  treemacs-expand-after-init               t
+	  treemacs-git-command-pipe                ""
+	  treemacs-goto-tag-strategy               'refetch-index
+	  treemacs-indentation                     2
+	  treemacs-indentation-string              " "
+	  treemacs-is-never-other-window           nil
+	  treemacs-max-git-entries                 5000
+	  treemacs-missing-project-action          'ask
+	  treemacs-move-forward-on-expand          nil
+	  treemacs-no-png-images                   nil
+	  treemacs-no-delete-other-windows         t
+	  treemacs-project-follow-cleanup          nil
+	  treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
+	  treemacs-position                        'left
+	  treemacs-read-string-input               'from-child-frame
+	  treemacs-recenter-distance               0.1
+	  treemacs-recenter-after-file-follow      nil
+	  treemacs-recenter-after-tag-follow       nil
+	  treemacs-recenter-after-project-jump     'always
+	  treemacs-recenter-after-project-expand   'on-distance
+	  treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
+	  treemacs-show-cursor                     nil
+	  treemacs-show-hidden-files               t
+	  treemacs-silent-filewatch                nil
+	  treemacs-silent-refresh                  nil
+	  treemacs-sorting                         'alphabetic-asc
+	  treemacs-select-when-already-in-treemacs 'move-back
+	  treemacs-space-between-root-nodes        t
+	  treemacs-tag-follow-cleanup              t
+	  treemacs-tag-follow-delay                1.5
+	  treemacs-text-scale                      nil
+	  treemacs-user-mode-line-format           nil
+	  treemacs-user-header-line-format         nil
+	  treemacs-wide-toggle-width               70
+	  treemacs-width                           35
+	  treemacs-width-increment                 1
+	  treemacs-width-is-initially-locked       t
+	  treemacs-workspace-switch-cleanup        nil)
+
+    ;; The default width and height of the icons is 22 pixels. If you are
+    ;; using a Hi-DPI display, uncomment this to double the icon size.
+    ;;(treemacs-resize-icons 44)
+
+    (treemacs-follow-mode t)
+    (treemacs-filewatch-mode t)
+    (treemacs-fringe-indicator-mode 'always)
+
+    (pcase (cons (not (null (executable-find "git")))
+		 (not (null treemacs-python-executable)))
+      (`(t . t)
+       (treemacs-git-mode 'deferred))
+      (`(t . _)
+       (treemacs-git-mode 'simple))))
+  :bind
+  (:map global-map
+	("M-0"       . treemacs-select-window)
+	("C-x t 1"   . treemacs-delete-other-windows)
+	("C-x t t"   . treemacs)
+	("C-x t B"   . treemacs-bookmark)
+	("C-x t C-t" . treemacs-find-file)
+	("C-x t M-t" . treemacs-find-tag)))
